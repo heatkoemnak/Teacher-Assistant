7a8b608d74c5c2589bbf340295a1fb36942de736
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-05-07 11:14:50 +0700

    Feat: Creating Vue Components

---
+++ teacher-asisst-frontend/src/pages/admin/components/taBtn.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-col cols="12" md="4" sm="6">
+    <v-btn rounded="xl" size="x-large" block :color="color">Rounded xl</v-btn>
+  </v-col>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  color: {
+    type: String,
+    default: '#E84D72'
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
