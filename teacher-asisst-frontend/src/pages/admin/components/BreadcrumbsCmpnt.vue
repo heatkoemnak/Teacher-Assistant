6ec6928e09e73a827b370159f2b1cf8fa5a81778
Heat Koemnak <heatkimnak@gmail.com>
2024-05-23 13:07:36 +0700

    modified breadcrumb

--- teacher-asisst-frontend/src/pages/admin/components/BreadcrumbsCmpnt.vue
+++ teacher-asisst-frontend/src/pages/admin/components/BreadcrumbsCmpnt.vue
@@ -33,7 +33,6 @@ export default {
       () => route.path,
       (val) => {
         currentRouteUrl.value = val;
-        console.log(val);
       }
     );
     // const updateCurrentRoutePath = () => {
