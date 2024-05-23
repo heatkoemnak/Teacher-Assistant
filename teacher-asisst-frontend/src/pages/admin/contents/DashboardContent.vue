706ca32d4ace75709689ffde7c5706170a744642
Heat Koemnak <heatkimnak@gmail.com>
2024-05-23 13:06:47 +0700

    modified dashboard content

--- teacher-asisst-frontend/src/pages/admin/contents/DashboardContent.vue
+++ teacher-asisst-frontend/src/pages/admin/contents/DashboardContent.vue
@@ -3,19 +3,21 @@
     <v-row justify="center">
       <v-col cols="12" sm="9" md="3" v-for="card in cards" :key="card.id">
         <v-card
-          class="text-white pointer-card"
+          class="text-white d-flex pointer-card"
           :style="{ background: card.backgroundGradient }"
           :to="card.route"
+          max-width="260"
+          height="130"
+          hover
         >
-          <v-icon class="ml-3 mt-2" size="48">{{ card.icon }}</v-icon>
-          <v-card-title>
-            {{ card.title }}
-          </v-card-title>
-          <v-card-actions>
-            <v-btn color="white" style="font-size: 25px">250</v-btn>
-            <v-spacer></v-spacer>
-            <v-btn color="secondary">More info</v-btn>
-          </v-card-actions>
+          <div>
+            <v-icon class="ml-3 mt-2" size="48">{{ card.icon }}</v-icon>
+            <v-card-title>
+              {{ card.title }}
+            </v-card-title>
+          </div>
+          <v-spacer></v-spacer>
+          <v-card-title> 250 </v-card-title>
         </v-card>
       </v-col>
     </v-row>
@@ -42,36 +44,35 @@ export default {
         id: 2,
         title: "Students",
         icon: "mdi-account-group",
-        backgroundGradient: "linear-gradient(45deg, #1D2B64, #1D2B64)",
+        backgroundGradient: "linear-gradient(45deg, #00bca1,#00bca1)",
         route: "/admin/management/students",
       },
       {
         id: 3,
         title: "Classes",
         icon: "mdi-school",
-        backgroundGradient: "linear-gradient(45deg, #FF416C, #FF416D)",
+        backgroundGradient: "linear-gradient(45deg, #cfad00, #cfad00)",
         route: "/admin/management/classes",
       },
       {
         id: 4,
         title: "Attendance",
         icon: "mdi-calendar-check",
-        backgroundGradient: "linear-gradient(45deg, #1D2B64, #1D2B63)",
+        backgroundGradient: "linear-gradient(45deg, #4CC9F0, #4CC9F0)",
         route: "/admin/management/attendance",
       },
       {
         id: 5,
         title: "Departments",
         icon: "mdi-domain",
-        backgroundGradient: "linear-gradient(45deg, #363795, #363795)",
+        backgroundGradient: "linear-gradient(45deg, #007498, #007498)",
         route: "/admin/management/departments",
-
       },
       {
         id: 6,
         title: "Grading",
         icon: "mdi-star",
-        backgroundGradient: "linear-gradient(45deg, #D31027, #EA384D)",
+        backgroundGradient: "linear-gradient(45deg, #1e4572, #1e4572)",
       },
       {
         id: 7,
@@ -83,7 +84,7 @@ export default {
         id: 8,
         title: "Notifications",
         icon: "mdi-bell",
-        backgroundGradient: "linear-gradient(45deg, #16222A, #3A6073)",
+        backgroundGradient: "linear-gradient(45deg, #00bca1,#00a3a4)",
       },
     ]);
     return { cards };
