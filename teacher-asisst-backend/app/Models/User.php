102e9a549c79815b7de5bb04caf55e33b161fdad
Heat Koemnak <heatkimnak@gmail.com>
2024-05-23 23:18:18 +0700

    create user and profile model

--- teacher-asisst-backend/app/Models/User.php
+++ teacher-asisst-backend/app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -43,5 +44,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
     }
 }
